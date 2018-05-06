8ae22247428ab1a407d04f069461a3670e91e871
Alex Becker <acbecker@uchicago.edu>
2018-05-05 21:57:53 -0700

    packagify

---
+++ README.rst
@@ -0,0 +1,11 @@
+dotlock
+=======
+
+Bringing sanity to Python package management.
+
+Usage
+-----
+
+.. code-block:: shell
+    dotlock init  # Creates a virtualenv in the current working directory.
+    dotlock lock  # Generates a package.lock.json file from package.json.
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+
+with open('dotlock/__init__.py') as fp:
+    # defines __version__
+    exec(fp.read())
+
+
+with open('README.rst') as fp:
+    README = fp.read()
+
+
+setup(
+    name='dotlock',
+    version=__version__,
+    author='Alex Becker',
+    author_email='myself@alexcbecker.net',
+    url='https://github.com/alexbecker/dotlock',
+    description='Fast and accurate Python dependency management',
+    long_description=README,
+    tests_require=[
+        'pytest',
+        'pytest-asyncio',
+    ],
+    py_modules=['dotlock'],
+    install_requires=[
+        'aiohttp>=3.1',
+        'distlib',
+        'packaging',
+        'pip>=9.0',
+        'setuptools>=39.0',
+        'virtualenv',
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='packaging requirements dependencies',
+)
