d3b2edf6c8c2f48480b9fa93e4cd9309d006a402
Alex Becker <acbecker@uchicago.edu>
2018-06-10 22:48:28 -0700

    add end-to-end test for simple API; bypass cache in end-to-end tests

--- tests/integration/test_end_to_end.py
+++ tests/integration/test_end_to_end.py
@@ -1,29 +1,41 @@
 import json
 import subprocess
 
+import pytest
+
 from dotlock.__main__ import _main
 from dotlock.tempdir import temp_working_dir
 
 
-def test_requests():
+@pytest.mark.parametrize('source,package_name,package_version', [
+    ('https://pypi.org/pypi', 'requests', '2.18.4'),
+    ('https://pypi.org/simple', 'requests', '2.18.3'),
+])
+def test_single_package(source, package_name, package_version):
     with temp_working_dir():
+        import logging; logging.getLogger('dotlock').setLevel(logging.DEBUG)
         assert _main('init') == 0
 
         with open('package.json') as fp:
             package_json = json.load(fp)
 
-        package_json['default']['requests'] = '==2.18.4'
+        package_json['sources'] = [source]
+        package_json['default'][package_name] = f'=={package_version}'
         with open('package.json', 'w') as fp:
             json.dump(package_json, fp)
 
-        assert _main('lock') == 0
+        # Use --update to bypass the cache.
+        assert _main('lock', '--update') == 0
         assert _main('install') == 0
 
         # We have to run "run" in a subprocess so it doesn't hijack test execution.
         run_process = subprocess.run(
-            ['dotlock', 'run', 'python', '-c', 'import requests; print(requests.__version__)'],
+            [
+                'dotlock', 'run', 'python', '-c',
+                f'import {package_name}; print({package_name}.__version__)',
+            ],
             stdout=subprocess.PIPE,
         )
 
         output = run_process.stdout.decode('utf-8').strip()
-        assert output == '2.18.4'
+        assert output == package_version
