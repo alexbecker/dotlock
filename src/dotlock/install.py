a7e64823d9d927766012b44455c99681119c9487
Alex Becker <acbecker@uchicago.edu>
2018-09-07 20:55:37 -0700

    refactor: install(candidates), not install(requirements)

--- src/dotlock/install.py
+++ src/dotlock/install.py
@@ -15,43 +15,43 @@ from dotlock.tempdir import temp_working_dir
 logger = logging.getLogger(__name__)
 
 
-async def download(session: ClientSession, requirement: dict):
-    url = requirement['url']
-    vcs_url = requirement['vcs_url']
+async def download(session: ClientSession, candidate: dict):
+    url = candidate['url']
+    vcs_url = candidate['vcs_url']
     if vcs_url:
-        logger.info('Cloning %s from %s', requirement['name'], vcs_url)
-        clone_dir_name = await clone(requirement['vcs_url'])
+        logger.info('Cloning %s from %s', candidate['name'], vcs_url)
+        clone_dir_name = await clone(candidate['vcs_url'])
         # Rename the cloned directory so it is unique and easy to install from.
-        os.rename(clone_dir_name, requirement['name'])
+        os.rename(clone_dir_name, candidate['name'])
     else:
-        logger.info('Downloading %s from %s', requirement['name'], url)
+        logger.info('Downloading %s from %s', candidate['name'], url)
 
-        async with session.get(requirement['url']) as response:
+        async with session.get(candidate['url']) as response:
             response.raise_for_status()
             # TODO: download in chunks to reduce memory usage
             contents = await response.read()
         digest = sha256(contents).hexdigest()
-        if digest != requirement['sha256']:
-            raise HashMismatchError(requirement['name'], requirement['version'], digest, requirement['sha256'])
+        if digest != candidate['sha256']:
+            raise HashMismatchError(candidate['name'], candidate['version'], digest, candidate['sha256'])
 
-        package_filename = requirement['url'].split('/')[-1]
+        package_filename = candidate['url'].split('/')[-1]
         with open(package_filename, 'wb') as fp:
             fp.write(contents)
 
 
-async def download_all(requirements: List[dict]):
+async def download_all(candidates: List[dict]):
     async with ClientSession() as session:
         return await asyncio.gather(*[
-            download(session, requirement) for requirement in requirements
+            download(session, candidate) for candidate in candidates
         ])
 
 
-async def install(requirements: List[dict]):
+async def install(candidates: List[dict]):
     venv_path = os.path.join(os.getcwd(), 'venv')
 
     with temp_working_dir('install'):
-        await download_all(requirements)
-        for requirement in requirements:
+        await download_all(candidates)
+        for candidate in candidates:
             args = [
                 'pip', 'install',
                 # Specify venv as the install target.
@@ -67,13 +67,13 @@ async def install(requirements: List[dict]):
                 # See https://github.com/pypa/pip/issues/5402 for discussion.
                 '--no-build-isolation',
             ]
-            if requirement['vcs_url']:
-                target_name = f'./{requirement["name"]}'
+            if candidate['vcs_url']:
+                target_name = f'./{candidate["name"]}'
             else:
-                # We can't just use requirement['name'] as the package name because
+                # We can't just use candidate['name'] as the package name because
                 # pip won't find the file if its (potentially non-canonical) name
                 # does not match the package name.
-                target_name = requirement['url'].split('/')[-1]
+                target_name = candidate['url'].split('/')[-1]
             args.append(target_name)
             logger.debug(' '.join(args))
             process = await asyncio.subprocess.create_subprocess_exec(*args)
